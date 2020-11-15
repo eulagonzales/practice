4617c820d97af8876a3823e3b382e7325756419a
Eula Gonzales <45725937+eulagonzales@users.noreply.github.com>
2020-11-15 17:48:19 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//Typewriter Effect
+var typed2 = new Typed('.typer', {
+    strings: ['Hello', 'Hola', 'Olá', 'Salut', 'Ciao', 'Hallo', 'Hej'],
+    typeSpeed: 0,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: true
+});
